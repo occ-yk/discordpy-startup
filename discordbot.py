67b129a31f5960f808b5c771763d1fb24d0fa548
occ-yk <79461369+occ-yk@users.noreply.github.com>
2021-02-23 08:11:44 +0900

    command to !

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -15,7 +15,6 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
-    await ctx.send('by occ')
+    await ctx.send('pong by occ')
 
 bot.run(token)
